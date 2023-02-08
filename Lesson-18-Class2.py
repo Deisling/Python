01915382cc7c077e7bc591e3d3dc59f77f98b225
Deisling <13deisling@gmail.com>
2023-02-08 21:26:23 +0100

    18

---
+++ Lesson-18-Class2.py
@@ -0,0 +1,18 @@
+from hero import *
+
+myhero = Hero("Vurdalak", 4, "Orc")
+mysuperhero = SuperHero("Moisey", 10, "Human", 5)
+
+myhero.show_hero()
+mysuperhero.show_hero()
+
+mysuperhero.makemagic()
+mysuperhero.show_hero()
+
+mysuperhero.makemagic()
+mysuperhero.makemagic()
+mysuperhero.show_hero()
+
+# Проверка с двумя андеркорами, так пишем, когда хотим, чтобы не менялась функция, метод 
+mysuperhero.__magic = 10 
+mysuperhero.show_hero() 
