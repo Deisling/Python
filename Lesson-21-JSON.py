0b40d6f87fe5213ad17c4e5aeea1f8ae518d3766
Deisling <13deisling@gmail.com>
2023-02-11 20:15:32 +0100

    21

---
+++ Lesson-21-JSON.py
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+import json
+filename = "user_settings.txt"
+myfile = open(filename, mode='w', encoding='Latin=1')
+
+
+player1 = {
+    'PlayerName': "Donald Trump",
+    'Score': 345,
+    'Awards': ["OR", "NV", "NY"]   
+}
+
+player2 = {
+    'PlayerName': "Clinton Hillary",
+    'Score': 346,
+    'Awards': ["WI", "TX", "MI"]   
+}
+
+myplayers = []
+myplayers.append(player1)
+myplayers.append(player2)
+
+ # -------------- SAVE by JSON ---------------
+
+json.dump(myplayers, myfile)
+myfile.close()
+
+
+# --------- LOAD by JSON -------------------
+
+myfile = open(filename, mode='r', encoding='Latin-1')
+json_moyainformaziya = json.load(myfile)
+
+for user in json_moyainformaziya:
+    print("Player Name is: " + str(user['PlayerName']))
+    print("Player Score is: " + str(user['Score']))
+    print("Player Award N1: " + str(user['Awards'][0]))
+    print("Player Award N2: " + str(user['Awards'][1]))
+    print("Player Award N2: " + str(user['Awards'][2]))
+    print("------------------------------------------\n\n")
+    
